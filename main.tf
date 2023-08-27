8bbfed23aafa07c7ed86dba5eb21c30d3409a81a
Swami <137131326+aws-user-forever@users.noreply.github.com>
2023-08-27 21:50:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "ec2" {
+  ami           = "ami-05fa00d4c63e32376" # us-east-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Terraform-Provisioned-Instance"
   }
 }
